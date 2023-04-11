935aee46d13a35675ed5c8cb0b0d018bfef2d6bd
ThisIsPhila <pmteshani@gmail.com>
2023-04-11 10:59:35 +0200

    Simplified Code

--- client/counter.js
+++ client/counter.js
@@ -1,9 +1,23 @@
-export function setupCounter(element) {
+// This function takes an element as a parameter and sets up a counter that increments when the element is clicked.
+function setupCounter(element) {
   let counter = 0
-  const setCounter = (count) => {
+
+  // This function sets the counter to the specified count.
+  function setCounter(count) {
     counter = count
     element.innerHTML = `count is ${counter}`
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
+
+  // This function increments the counter by 1.
+  function incrementCounter() {
+    setCounter(counter + 1)
+  }
+
+  // This function adds a click event listener to the element.
+  function addHandler() {
+    element.addEventListener('click', incrementCounter)
+  }
+
+  addHandler()
   setCounter(0)
 }
